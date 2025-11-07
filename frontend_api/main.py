f75bee119df33f8ba9e2fc92983e9cc27ba7ab3b
Kamil Pustelnik <kpustelnik@student.agh.edu.pl>
2025-11-07 11:34:44 +0100

    Add CORS middleware

--- frontend_api/main.py
+++ frontend_api/main.py
@@ -1,6 +1,7 @@
 import os
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
 from starlette.staticfiles import StaticFiles
 
@@ -13,6 +14,16 @@ description = f'''
 **Build from:** {os.getenv('BUILD_TIME', "unknown")} rev. {os.getenv("CI_COMMIT_SHORT_SHA", "unknown")}.
 '''
 app = FastAPI(lifespan=lifespan, title="IoT Frontend API", openapi_tags=tags_metadata, description=description)
+
+origins = ["http://localhost:3000", "https://wihajster-front.ivk.pl"] # TODO: Clean
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
