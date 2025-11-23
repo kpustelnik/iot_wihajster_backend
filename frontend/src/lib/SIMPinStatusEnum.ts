7cab30a52d9d164679d82f2d32337ea298abc6cc
Kamil Pustelnik <kpustelnik@student.agh.edu.pl>
2025-11-23 15:13:10 +0100

    add no sim pin status

--- frontend/src/lib/SIMPinStatusEnum.ts
+++ frontend/src/lib/SIMPinStatusEnum.ts
@@ -4,7 +4,8 @@ enum SIMPinStatusEnum {
   PIN_REQUIRED = 2,
   PUK_REQUIRED = 3,
   PH_SIM_PIN_REQUIRED = 4,
-  PH_NET_PIN_REQUIRED = 5
+  PH_NET_PIN_REQUIRED = 5,
+  SIM_NOT_INSERTED = 6,
 }
 
 export default SIMPinStatusEnum;
