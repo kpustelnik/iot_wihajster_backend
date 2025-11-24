01cb33198f7576c97c5a10403806875bc0491bbf
Kamil Pustelnik <kpustelnik@student.agh.edu.pl>
2025-11-24 02:59:00 +0100

    add logger handler

--- app_common/utils/mqtt_handler.py
+++ app_common/utils/mqtt_handler.py
@@ -23,8 +23,12 @@ def create_tls_context():
 
 TLS_CONTEXT = create_tls_context()
 
-
 logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[logging.StreamHandler()]
+)
 
 MQTT_HOST = "mqtt_ext"
 MQTT_PORT = 2883
