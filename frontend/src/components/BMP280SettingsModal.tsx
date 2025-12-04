947dd5505ffb58fd63c31232c517dae59856c63f
Kamil Pustelnik <kpustelnik@student.agh.edu.pl>
2025-12-04 15:52:24 +0100

    fix the value and add grid

--- frontend/src/components/BMP280SettingsModal.tsx
+++ frontend/src/components/BMP280SettingsModal.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Typography, Box, Button, CircularProgress, FormControl, Skeleton, RadioGroup, Radio, FormControlLabel, FormLabel } from "@mui/material";
+import { Typography, Box, Button, CircularProgress, FormControl, Skeleton, RadioGroup, Radio, FormControlLabel, FormLabel, Grid } from "@mui/material";
 
 import { BluetoothQueueContext } from '@/components/BluetoothQueueProvider';
 
@@ -57,70 +57,80 @@ export default function BMP280SettingsModal({ open, onClose, server }: {
           <Skeleton variant="rectangular" width={400} height={200} sx={{ m: 2 }} />
         ) : (
           <>
-            <FormControl>
-              <FormLabel id="bmp280-pressure-oversampling">Pressure oversampling</FormLabel>
-              <RadioGroup
-                value={newBMP280Settings.toString()}
-                onChange={(e) => setNewBMP280Settings({ ...newBMP280Settings, oversamplingPress: parseInt(e.target.value) })}
-                name="bmp280-pressure-oversampling"
-              >
-                <FormControlLabel value={0b0} control={<Radio />} label="Disabled" />
-                <FormControlLabel value={0b1} control={<Radio />} label="x1" />
-                <FormControlLabel value={0b10} control={<Radio />} label="x2" />
-                <FormControlLabel value={0b11} control={<Radio />} label="x4" />
-                <FormControlLabel value={0b100} control={<Radio />} label="x8" />
-                <FormControlLabel value={0b101} control={<Radio />} label="x16" />
-              </RadioGroup>
-            </FormControl>
+            <Grid container spacing={2}>
+              <Grid size={{ xs: 12, sm: 6}}>
+                <FormControl>
+                  <FormLabel id="bmp280-pressure-oversampling">Pressure oversampling</FormLabel>
+                  <RadioGroup
+                    value={newBMP280Settings.oversamplingPress}
+                    onChange={(e) => setNewBMP280Settings({ ...newBMP280Settings, oversamplingPress: parseInt(e.target.value) })}
+                    name="bmp280-pressure-oversampling"
+                  >
+                    <FormControlLabel value={0b0} control={<Radio />} label="Disabled" />
+                    <FormControlLabel value={0b1} control={<Radio />} label="x1" />
+                    <FormControlLabel value={0b10} control={<Radio />} label="x2" />
+                    <FormControlLabel value={0b11} control={<Radio />} label="x4" />
+                    <FormControlLabel value={0b100} control={<Radio />} label="x8" />
+                    <FormControlLabel value={0b101} control={<Radio />} label="x16" />
+                  </RadioGroup>
+                </FormControl>
+              </Grid>
 
-            <FormControl>
-              <FormLabel id="bmp280-temperature-oversampling">Temperature oversampling</FormLabel>
-              <RadioGroup
-                value={newBMP280Settings.toString()}
-                onChange={(e) => setNewBMP280Settings({ ...newBMP280Settings, oversamplingTemp: parseInt(e.target.value) })}
-                name="bmp280-temperature-oversampling"
-              >
-                <FormControlLabel value={0b0} control={<Radio />} label="Disabled" />
-                <FormControlLabel value={0b1} control={<Radio />} label="x1" />
-                <FormControlLabel value={0b10} control={<Radio />} label="x2" />
-                <FormControlLabel value={0b11} control={<Radio />} label="x4" />
-                <FormControlLabel value={0b100} control={<Radio />} label="x8" />
-                <FormControlLabel value={0b101} control={<Radio />} label="x16" />
-              </RadioGroup>
-            </FormControl>
+              <Grid size={{ xs: 12, sm: 6}}>
+                <FormControl>
+                  <FormLabel id="bmp280-temperature-oversampling">Temperature oversampling</FormLabel>
+                  <RadioGroup
+                    value={newBMP280Settings.oversamplingTemp}
+                    onChange={(e) => setNewBMP280Settings({ ...newBMP280Settings, oversamplingTemp: parseInt(e.target.value) })}
+                    name="bmp280-temperature-oversampling"
+                  >
+                    <FormControlLabel value={0b0} control={<Radio />} label="Disabled" />
+                    <FormControlLabel value={0b1} control={<Radio />} label="x1" />
+                    <FormControlLabel value={0b10} control={<Radio />} label="x2" />
+                    <FormControlLabel value={0b11} control={<Radio />} label="x4" />
+                    <FormControlLabel value={0b100} control={<Radio />} label="x8" />
+                    <FormControlLabel value={0b101} control={<Radio />} label="x16" />
+                  </RadioGroup>
+                </FormControl>
+              </Grid>
 
-            <FormControl>
-              <FormLabel id="bmp280-standby">Standby time</FormLabel>
-              <RadioGroup
-                value={newBMP280Settings.toString()}
-                onChange={(e) => setNewBMP280Settings({ ...newBMP280Settings, standbyTime: parseInt(e.target.value) })}
-                name="bmp280-standby"
-              >
-                <FormControlLabel value={0b0} control={<Radio />} label="0.5 ms" />
-                <FormControlLabel value={0b1} control={<Radio />} label="62.5 ms" />
-                <FormControlLabel value={0b10} control={<Radio />} label="125 ms" />
-                <FormControlLabel value={0b11} control={<Radio />} label="250 ms" />
-                <FormControlLabel value={0b100} control={<Radio />} label="500 ms" />
-                <FormControlLabel value={0b101} control={<Radio />} label="1000 ms" />
-                <FormControlLabel value={0b110} control={<Radio />} label="2000 ms" />
-                <FormControlLabel value={0b111} control={<Radio />} label="4000 ms" />
-              </RadioGroup>
-            </FormControl>
+              <Grid size={{ xs: 12, sm: 6}}>
+                <FormControl>
+                  <FormLabel id="bmp280-standby">Standby time</FormLabel>
+                  <RadioGroup
+                    value={newBMP280Settings.standbyTime}
+                    onChange={(e) => setNewBMP280Settings({ ...newBMP280Settings, standbyTime: parseInt(e.target.value) })}
+                    name="bmp280-standby"
+                  >
+                    <FormControlLabel value={0b0} control={<Radio />} label="0.5 ms" />
+                    <FormControlLabel value={0b1} control={<Radio />} label="62.5 ms" />
+                    <FormControlLabel value={0b10} control={<Radio />} label="125 ms" />
+                    <FormControlLabel value={0b11} control={<Radio />} label="250 ms" />
+                    <FormControlLabel value={0b100} control={<Radio />} label="500 ms" />
+                    <FormControlLabel value={0b101} control={<Radio />} label="1000 ms" />
+                    <FormControlLabel value={0b110} control={<Radio />} label="2000 ms" />
+                    <FormControlLabel value={0b111} control={<Radio />} label="4000 ms" />
+                  </RadioGroup>
+                </FormControl>
+              </Grid>
 
-            <FormControl>
-              <FormLabel id="bmp280-filter">Filter</FormLabel>
-              <RadioGroup
-                value={newBMP280Settings.toString()}
-                onChange={(e) => setNewBMP280Settings({ ...newBMP280Settings, filter: parseInt(e.target.value) })}
-                name="bmp280-filter"
-              >
-                <FormControlLabel value={0b0} control={<Radio />} label="OFF" />
-                <FormControlLabel value={0b1} control={<Radio />} label="2" />
-                <FormControlLabel value={0b10} control={<Radio />} label="4" />
-                <FormControlLabel value={0b11} control={<Radio />} label="8" />
-                <FormControlLabel value={0b100} control={<Radio />} label="16" />
-              </RadioGroup>
-            </FormControl>
+              <Grid size={{ xs: 12, sm: 6}}>
+                <FormControl>
+                  <FormLabel id="bmp280-filter">Filter</FormLabel>
+                  <RadioGroup
+                    value={newBMP280Settings.filter}
+                    onChange={(e) => setNewBMP280Settings({ ...newBMP280Settings, filter: parseInt(e.target.value) })}
+                    name="bmp280-filter"
+                  >
+                    <FormControlLabel value={0b0} control={<Radio />} label="OFF" />
+                    <FormControlLabel value={0b1} control={<Radio />} label="2" />
+                    <FormControlLabel value={0b10} control={<Radio />} label="4" />
+                    <FormControlLabel value={0b11} control={<Radio />} label="8" />
+                    <FormControlLabel value={0b100} control={<Radio />} label="16" />
+                  </RadioGroup>
+                </FormControl>
+              </Grid>
+            </Grid>
           </>
         )
       }
