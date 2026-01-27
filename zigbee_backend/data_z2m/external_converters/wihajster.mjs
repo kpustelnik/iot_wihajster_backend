ec3d3c6b55405752bfbb4d82cadb47b781785d7a
Kamil Pustelnik <kpustelnik@student.agh.edu.pl>
2026-01-27 02:24:22 +0100

    last fix

--- zigbee_backend/data_z2m/external_converters/wihajster.mjs
+++ zigbee_backend/data_z2m/external_converters/wihajster.mjs
@@ -178,7 +178,7 @@ export default {
             voltage: true,
             lowStatus: false,
             percentageReporting: true,
-            voltageReporting: false,
+            voltageReporting: true,
             dontDividePercentage: false
         }),
 
