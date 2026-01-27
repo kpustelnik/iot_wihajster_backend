bfb5952febce010a55ba870cef5984d0c4acc1a6
Kamil Pustelnik <kpustelnik@student.agh.edu.pl>
2026-01-27 02:10:22 +0100

    fixed converter

--- zigbee_backend/data_z2m/external_converters/wihajster.mjs
+++ zigbee_backend/data_z2m/external_converters/wihajster.mjs
@@ -1,18 +1,138 @@
 import * as m from 'zigbee-herdsman-converters/lib/modernExtend';
 import {Zcl} from "zigbee-herdsman";
+import * as exposes from 'zigbee-herdsman-converters/lib/exposes';
+const e = exposes.presets;
+const ea = exposes.access;
+
+// BMP280 settings bit layout (uint16):
+// bits 0-2:  pressure oversampling (pres_os)
+// bits 3-5:  temperature oversampling (temp_os)
+// bits 6-8:  filter coefficient
+// bits 9-11: standby time
+
+const BMP280_STANDBY_LOOKUP = {
+    "0.5ms": 0b000,
+    "62.5ms": 0b001,
+    "125ms": 0b010,
+    "250ms": 0b011,
+    "500ms": 0b100,
+    "1000ms": 0b101,
+    "2000ms": 0b110,
+    "4000ms": 0b111
+};
+
+const BMP280_FILTER_LOOKUP = {
+    "off": 0b000,
+    "2": 0b001,
+    "4": 0b010,
+    "8": 0b011,
+    "16": 0b100
+};
+
+const BMP280_OVERSAMPLING_LOOKUP = {
+    "disabled": 0b000,
+    "x1": 0b001,
+    "x2": 0b010,
+    "x4": 0b011,
+    "x8": 0b100,
+    "x16": 0b101
+};
+
+// Reverse lookups
+const BMP280_STANDBY_REVERSE = Object.fromEntries(Object.entries(BMP280_STANDBY_LOOKUP).map(([k, v]) => [v, k]));
+const BMP280_FILTER_REVERSE = Object.fromEntries(Object.entries(BMP280_FILTER_LOOKUP).map(([k, v]) => [v, k]));
+const BMP280_OVERSAMPLING_REVERSE = Object.fromEntries(Object.entries(BMP280_OVERSAMPLING_LOOKUP).map(([k, v]) => [v, k]));
+
+// Custom fromZigbee converter for BMP280 settings
+const fzBmp280Settings = {
+    cluster: 'manuSpecificWihajster',
+    type: ['attributeReport', 'readResponse'],
+    convert: (model, msg, publish, options, meta) => {
+        const result = {};
+        if (msg.data.hasOwnProperty('bmp280Settings')) {
+            const settings = msg.data.bmp280Settings;
+            const presOs = settings & 0b111;
+            const tempOs = (settings >> 3) & 0b111;
+            const filter = (settings >> 6) & 0b111;
+            const standby = (settings >> 9) & 0b111;
+            
+            result.bmp280_settings = settings;
+            result.bmp280_pressure_oversampling = BMP280_OVERSAMPLING_REVERSE[presOs] || 'unknown';
+            result.bmp280_temp_oversampling = BMP280_OVERSAMPLING_REVERSE[tempOs] || 'unknown';
+            result.bmp280_filter = BMP280_FILTER_REVERSE[filter] || 'unknown';
+            result.bmp280_standby_time = BMP280_STANDBY_REVERSE[standby] || 'unknown';
+        }
+        return result;
+    },
+};
+
+// Custom toZigbee converter for BMP280 settings
+const tzBmp280Settings = {
+    key: ['bmp280_settings', 'bmp280_pressure_oversampling', 'bmp280_temp_oversampling', 'bmp280_filter', 'bmp280_standby_time'],
+    convertSet: async (entity, key, value, meta) => {
+        // First read current settings
+        const endpoint = entity;
+        let currentSettings = 0;
+        
+        try {
+            const response = await endpoint.read('manuSpecificWihajster', ['bmp280Settings']);
+            currentSettings = response.bmp280Settings || 0;
+        } catch (e) {
+            // If read fails, start from 0
+        }
+        
+        let presOs = currentSettings & 0b111;
+        let tempOs = (currentSettings >> 3) & 0b111;
+        let filter = (currentSettings >> 6) & 0b111;
+        let standby = (currentSettings >> 9) & 0b111;
+        
+        if (key === 'bmp280_settings') {
+            // Direct raw value set
+            const newSettings = value;
+            await endpoint.write('manuSpecificWihajster', {bmp280Settings: newSettings});
+            return {state: {bmp280_settings: newSettings}};
+        } else if (key === 'bmp280_pressure_oversampling') {
+            presOs = BMP280_OVERSAMPLING_LOOKUP[value] ?? presOs;
+        } else if (key === 'bmp280_temp_oversampling') {
+            tempOs = BMP280_OVERSAMPLING_LOOKUP[value] ?? tempOs;
+        } else if (key === 'bmp280_filter') {
+            filter = BMP280_FILTER_LOOKUP[value] ?? filter;
+        } else if (key === 'bmp280_standby_time') {
+            standby = BMP280_STANDBY_LOOKUP[value] ?? standby;
+        }
+        
+        const newSettings = (standby << 9) | (filter << 6) | (tempOs << 3) | presOs;
+        await endpoint.write('manuSpecificWihajster', {bmp280Settings: newSettings});
+        
+        return {
+            state: {
+                bmp280_settings: newSettings,
+                bmp280_pressure_oversampling: BMP280_OVERSAMPLING_REVERSE[presOs],
+                bmp280_temp_oversampling: BMP280_OVERSAMPLING_REVERSE[tempOs],
+                bmp280_filter: BMP280_FILTER_REVERSE[filter],
+                bmp280_standby_time: BMP280_STANDBY_REVERSE[standby],
+            }
+        };
+    },
+    convertGet: async (entity, key, meta) => {
+        await entity.read('manuSpecificWihajster', ['bmp280Settings']);
+    },
+};
+
 export default {
     zigbeeModel: ['WIH-C6'],
     model: 'WIH-C6',
     vendor: 'WIHAJSTER',
     description: 'Automatically generated definition',
-    fromZigbee: [],
-    toZigbee: [],
+    fromZigbee: [fzBmp280Settings],
+    toZigbee: [tzBmp280Settings],
     extend: [
         m.deviceAddCustomCluster('manuSpecificWihajster', {
             ID: 0xfc00,
             attributes: {
                 //boot_count: {ID: 0x0001, type: Zcl.DataType.UINT16},
-                //heartbeat: {ID: 0x0002, type: Zcl.DataType.UINT8},            
+                //heartbeat: {ID: 0x0002, type: Zcl.DataType.UINT8},
+                bmp280Settings: {ID: 0x0030, type: Zcl.DataType.UINT16},
             },
             commands: {},
             commandsResponse: {},
@@ -25,8 +145,6 @@ export default {
             attribute: {ID: 0x0002, type: Zcl.DataType.UINT8},
             description: "Device operating mode",
         }),
-
-        // TODO: Add BMP280 Settings changing
 
         m.numeric({
             name: "boot_count",
@@ -120,81 +238,6 @@ export default {
             valueOn: [true, 1],
             valueOff: [false, 0],
             description: "Enable power management features",
-            access: "ALL"
-        }),
-
-        // BMP280 sensor configuration attributes
-        m.enumLookup({
-            name: "bmp280_mode",
-            lookup: {sleep: 0b00, forced: 0b01, normal: 0b11},
-            cluster: "manuSpecificWihajster",
-            attribute: {ID: 0x0030, type: Zcl.DataType.UINT8},
-            description: "BMP280 operating mode",
-            access: "ALL"
-        }),
-
-        m.enumLookup({
-            name: "bmp280_standby_time",
-            lookup: {
-                "0.5ms": 0b000,
-                "62.5ms": 0b001,
-                "125ms": 0b010,
-                "250ms": 0b011,
-                "500ms": 0b100,
-                "1000ms": 0b101,
-                "2000ms": 0b110,
-                "4000ms": 0b111
-            },
-            cluster: "manuSpecificWihajster",
-            attribute: {ID: 0x0038, type: Zcl.DataType.UINT8},
-            description: "BMP280 standby time between measurements in normal mode",
-            access: "ALL"
-        }),
-
-        m.enumLookup({
-            name: "bmp280_filter",
-            lookup: {
-                off: 0b000,
-                "2": 0b001,
-                "4": 0b010,
-                "8": 0b011,
-                "16": 0b100
-            },
-            cluster: "manuSpecificWihajster",
-            attribute: {ID: 0x0039, type: Zcl.DataType.UINT8},
-            description: "BMP280 IIR filter coefficient",
-            access: "ALL"
-        }),
-
-        m.enumLookup({
-            name: "bmp280_temp_oversampling",
-            lookup: {
-                disabled: 0b000,
-                x1: 0b001,
-                x2: 0b010,
-                x4: 0b011,
-                x8: 0b100,
-                x16: 0b101
-            },
-            cluster: "manuSpecificWihajster",
-            attribute: {ID: 0x003A, type: Zcl.DataType.UINT8},
-            description: "BMP280 temperature oversampling mode",
-            access: "ALL"
-        }),
-
-        m.enumLookup({
-            name: "bmp280_pressure_oversampling",
-            lookup: {
-                disabled: 0b000,
-                x1: 0b001,
-                x2: 0b010,
-                x4: 0b011,
-                x8: 0b100,
-                x16: 0b101
-            },
-            cluster: "manuSpecificWihajster",
-            attribute: {ID: 0x003B, type: Zcl.DataType.UINT8},
-            description: "BMP280 pressure oversampling mode",
             access: "ALL"
         }),
 
@@ -322,6 +365,18 @@ export default {
             valueMax: 255,
             access: "ALL"
         })
+    ],
+    exposes: [
+        e.numeric('bmp280_settings', ea.ALL)
+            .withDescription('BMP280 raw settings value (uint16)'),
+        e.enum('bmp280_standby_time', ea.ALL, ['0.5ms', '62.5ms', '125ms', '250ms', '500ms', '1000ms', '2000ms', '4000ms'])
+            .withDescription('BMP280 standby time between measurements'),
+        e.enum('bmp280_filter', ea.ALL, ['off', '2', '4', '8', '16'])
+            .withDescription('BMP280 IIR filter coefficient'),
+        e.enum('bmp280_temp_oversampling', ea.ALL, ['disabled', 'x1', 'x2', 'x4', 'x8', 'x16'])
+            .withDescription('BMP280 temperature oversampling'),
+        e.enum('bmp280_pressure_oversampling', ea.ALL, ['disabled', 'x1', 'x2', 'x4', 'x8', 'x16'])
+            .withDescription('BMP280 pressure oversampling'),
     ],
     meta: {},
 };
